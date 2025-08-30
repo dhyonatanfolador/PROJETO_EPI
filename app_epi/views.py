bc0cf809dd8ad1108e540adb211adc1c74f7ebe9
dhyonatan folador <dhyonatan.folador@gmail.com>
2025-08-30 08:39:20 -0300

    Corrigindo as rotas das páginas de início e sobre

--- app_epi/views.py
+++ app_epi/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "app_epi/partial/index.html")
+    return render(request, "app_epi/global/index.html")
 
 def inicio(request):
-    return render(request, "app_epi/partial/inicio.html")
+    return render(request, "app_epi/pages/inicio.html")
 
 def sobre(request):
-    return render(request, "app_epi/partial/sobre.html")
+    return render(request, "app_epi/pages/sobre.html")
